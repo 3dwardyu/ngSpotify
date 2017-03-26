88804397ca88c8764a2112c0909c7306941a394b
Edward Yu <3dward.yu@gmail.com>
2017-03-25 20:56:47 -0400

    edit albums page

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -4,7 +4,9 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SpotifyService{
-private searchUrl: string;
+    private searchUrl: string;
+    private artistUrl: string;
+    private albumsUrl: string;
 
     constructor(private _http:Http){
 
@@ -14,5 +16,17 @@ private searchUrl: string;
         this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=20&type='+type+'&market=CA';
         return this._http.get(this.searchUrl)
             .map(res => res.json());
+    }
+
+    getArtist(id:string){
+        this.artistUrl = "https://api.spotify.com/v1/artists/"+id;
+        return this._http.get(this.artistUrl)
+        .map(res => res.json());
+    }
+
+    getAlbums(artistId:string){
+        this.albumsUrl = "https://api.spotify.com/v1/artists/"+artistId+'/albums';
+        return this._http.get(this.albumsUrl)
+        .map(res => res.json());
     }
 }
